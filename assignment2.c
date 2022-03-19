2810837b938c3c3e613819ee3ea73add8db24fd2
Jon kehoe <67958536+jonkehoe@users.noreply.github.com>
2022-03-19 18:57:04 +0000

    commit 5

--- assignment2.c
+++ assignment2.c
@@ -1,3 +1,16 @@
+/*
+    Title: Assignment 2
+    Date: 14/03/2022
+    Author : Jon Kehoe C21372766
+    Program Description : This program is a lottory game that contains 5 differnt functions
+    1. Takes in 6 numbers from the user
+    2. Prints the numbers back to the user
+    3. Sorts the numbers from lowest to highest
+    4. Compares numbers with the winning numbers and see what the user won
+    5. Shows all numbers that have been entered and their frequency
+    6. Exits the game
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 6
@@ -30,6 +43,7 @@ int main()
         printf("3 - Sorted Numbers\n");
         printf("4 - Winnings\n");
         printf("5 - Display frequency\n");
+        printf("6 - Exit Game\n");
         scanf("%d", &num);
         d = getchar(); // checks to see if a charecter is input
 
@@ -40,20 +54,20 @@ int main()
 
             count++; // is used to check that number one is pressed first
 
-        } // end if
+        }                  // end if
         else if (num == 2) // second option
         {
             if (count > 0)
             {
                 // Displaying the numbers the user input
-                displaying = displayNumbers(numbers);
+                displaying = displayNumbers(userArray);
             }
             else
             {
                 printf("Need to press option 1 first\n");
             }
 
-        } // end else if
+        }                  // end else if
         else if (num == 3) // third option
         {
             if (count > 0)
@@ -66,7 +80,7 @@ int main()
                 printf("Need to press option 1 first\n");
             }
 
-        } // end else if
+        }                  // end else if
         else if (num == 4) // fourth option
         {
             if (count > 0)
@@ -79,7 +93,7 @@ int main()
                 printf("Need to press option 1 first\n");
             }
 
-        } // end else if
+        }                  // end else if
         else if (num == 5) // fifth option
         {
             if (count > 0)
@@ -93,10 +107,7 @@ int main()
             } // end else
 
         } // end else if
-        else
-        {
-            printf("Input error try again, Only numbers allowed\n");
-        } // end else
+        
 
     } while (num != 6 && d != (num >= 'a' && num <= 'z'));
     printf("Ending game\n");
@@ -107,57 +118,69 @@ int main()
 
 int *enterNumbers(int *numbersEntered, int *fArray)
 {
-    int temp;
-    int i, k;
+    int i, k, temp;
+    char d;
 
     printf("Enter %d numbers from 1 to %d and cant be the same number twice : ", SIZE, SIZEA);
     for (int i = 0; i < 6; i++)
     {
-        scanf("%d", &numbersEntered[i]);
-        // checks to see that numbers are greater than 0 and less than 43
+
+        scanf("%d", & *(numbersEntered + i));
+        d = getchar(); // checks to see if a charecter is input
+
         if (*(numbersEntered + i) > 0 && *(numbersEntered + i) < 43)
         {
-            for (k = 0; k < i; k++)
+            for (int k = 0; k < i; k++)
             {
                 // checks to see if number is input twice
                 if (*(numbersEntered + i) == *(numbersEntered + k))
                 {
-                    printf("You cant enter the same number twice\n");
+                    printf("You cant enter the same number twice!!\n");
+                    *(numbersEntered + i) = 0;
                     break;
 
                 } // end if
 
             } // end loop for same number
-        } // end if
+            if (*(numbersEntered + i) == 0)
+            {
+                printf("");
+                scanf("%d", & *(numbersEntered + i));
+            } // end if
+        }
         else
         {
-            // error checking for user input
-            printf("Numbers must be greater than 1 and less than 42\n");
-            printf("Returning to menu\n");
-            break;
+            printf("Only enter number 1 to 43\n");
+            *(numbersEntered + i) = 0;
         } // end else
+        if (*(numbersEntered + i) == 0) // used to enter a new number after error
+        {
+            printf("");
+            scanf("%d", & *(numbersEntered + i));
+        } // end if
 
         temp = *(numbersEntered + i);            // used to place last rounds numbers into a temp variable
         *(fArray + temp) = *(fArray + temp) + 1; // stores temp variable in a array for all past elements input
-    }// end for
+    }                                            // end for
     return numbersEntered;
 } // end enterNumber
 
 int *displayNumbers(int *display)
 {
-    int i; 
-
+    int i;
+    
+    printf("Numbers entered are : ");
     for (i = 0; i < SIZE; i++) // used to print numbers without a comma on the last element
     {
-        printf("%d", display[i]);
+        printf("%d", *(display + i));
         if (i < 5)
         {
             printf(", ");
         } // end if
-    } // end for
+    }     // end for
     printf("\n");
     return 0;
-}// end dispalyNumber
+} // end dispalyNumber
 
 void sortNumber(int array4[])
 {
@@ -167,12 +190,12 @@ void sortNumber(int array4[])
     {
         for (int j = i + 1; j < SIZE; j++)
         {
-            if (array4[i] > array4[j])
+            if (*(array4 + i) > *(array4 + j))
             {
                 // temp variable is used to store element that is being swapped
-                temp = array4[i];
-                array4[i] = array4[j];
-                array4[j] = temp;
+                temp = *(array4 + i);
+                *(array4 + i) = *(array4 + j);
+                *(array4 + j) = temp;
             }// end if
         } // end for
     } // end for
@@ -181,7 +204,7 @@ void sortNumber(int array4[])
 
     for (i = 0; i < SIZE; i++) // used to print numbers without a comma on the last element
     {
-        printf("%d", array4[i]);
+        printf("%d", *(array4 + i));
         if (i < 5)
         {
             printf(", ");
@@ -190,11 +213,12 @@ void sortNumber(int array4[])
     printf("\n");
 } // end sortNumber
 
+
 void compareArray(int *userArray, int *winningNumbers)
 {
 
-    int i, k, x, count;
-    int Length = 0;
+    int matched = 0;
+    int i,k;
 
     // looping throught both arrays for comparision of elements
     for (i = 0; i < 6; i++)
@@ -203,7 +227,8 @@ void compareArray(int *userArray, int *winningNumbers)
         {
             if (*(winningNumbers + k) == *(userArray + i))
             {
-                count++;
+                matched++;
+
             } // end if
 
         } // end for
@@ -211,28 +236,29 @@ void compareArray(int *userArray, int *winningNumbers)
     } // end out for
 
     // Used for Debug
-    // printf("%d",count);
+    // printf("%d",matched);
 
-    if (count == 6)
+    if (matched == 6)
     {
-        printf("You won the Jackpot!\n");
+        printf("You won the Jackpot\n");
     } // end if
-    else if (count == 5)
+    else if (matched == 5)
     {
         printf("You won a new Car\n");
     } // end else if
-    else if (count == 4)
+    else if (matched == 4)
     {
         printf("You won a Weekend Away!\n");
     } // end else if
-    else if (count == 3)
+    else if (matched == 3)
     {
-        printf("You won a Cinema Pass!\n");
+        printf("You won a Cinema Pass\n");
     } // end else if
     else
     {
-        printf("\nsorry you didnt win anything\n\n");
-    } // end else
+        printf("\nSorry you didnt win anything\n\n");
+    }//end else
+    
 
 } // end compareArray
 
